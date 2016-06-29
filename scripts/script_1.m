8eaa8a118dfb96386e44ad9b0d794127e5e8cd91
alexandros filotheou <alefil@kth.se>
2016-06-30 00:49:53 +0200

    Initial commit

---
+++ scripts/script_1.m
@@ -0,0 +1,25 @@
+clear
+close all
+
+%% The process's transfer function
+
+s=tf('s');
+
+G = 10^4 * (s+2) / ( (s+3) * (s+100)^2 );
+
+% Hinf(G);
+% [F_num, F_denom] = ss2tf(F.a, F.b, F.c, F.d)
+% Fsim = minreal(tf(F_num, F_denom))
+
+Gsim = G;
+
+% Load the controller determined by Hinf.
+% The weight Ws is 10^4 / ((s+a)(s+b))
+% where 
+% a = -0.1 + i * sqrt((100π)^2 - 0.1^2)
+% b = -0.1 - i * sqrt((100π)^2 - 0.1^2)
+load('Fsim_1.mat')
+Fsim = Fsim_1;
+
+%% Run the simulation
+macro
---
+++ scripts/script_3.m
@@ -0,0 +1,27 @@
+% clear
+% close all
+
+%% The process's transfer function
+
+s=tf('s');
+
+G = 10^4 * (s+2) * (s-1) / ( (s+3) * (s+2) * (s+100)^2 );
+
+% Hinf(G);
+% [F_num, F_denom] = ss2tf(F.a, F.b, F.c, F.d)
+% Fsim = minreal(tf(F_num, F_denom))
+
+Gsim = G;
+
+% Load the controller determined by Hinf.
+% The weight Ws is 10^4 / ((s+a)(s+b))
+% where 
+% a = -0.1 + i * sqrt((100π)^2 - 0.1^2)
+% b = -0.1 - i * sqrt((100π)^2 - 0.1^2)
+% The weight Wt is 3 / (s+2)
+% The weight Wu is the same as Ws
+load('Fsim_3.mat')
+Fsim = Fsim_3;
+
+%% Run the simulation
+macro
